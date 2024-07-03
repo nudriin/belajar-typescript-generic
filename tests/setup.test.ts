5d420a5206931428ebcc44c6dcd544de927fd4de
Nurdin <mrsunnysummer@gmail.com>
2024-07-03 11:32:46 +0700

    Setup

---
+++ tests/setup.test.ts
@@ -0,0 +1,9 @@
+describe('Setup test', () => {
+    it('should work as fine', () => {
+        const sayHello = (name: string): void => {
+            console.log(name);
+        };
+
+        sayHello('nurdin');
+    });
+});
